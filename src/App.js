a2bef217000088feab2c5af01c40707505336a7c
unknown <94donghoon.lee@gmail.com>
2021-01-04 22:44:37 +0900

    Add react router dom

--- src/App.js
+++ src/App.js
@@ -1,63 +1,15 @@
 import React from "react"
-import axios from "axios"
-import Movie from "./Movie"
-import "./App.css"
-import "./Home.css"
+import { HashRouter, Route } from "react-router-dom"
+import About from "./routes/About"
+import Home from "./routes/Home"
 
-// function App() {
-//   useEffect(() => {
-//     console.log("1234")
-//   })
-//   return <div className="App"></div>
-// }
-
-class App extends React.Component {
-  state = {
-    isLoading: true,
-    movies: [],
-  }
-  getMovies = async () => {
-    const {
-      data: {
-        data: { movies },
-      },
-    } = await axios.get(
-      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
-    )
-    this.setState({ movies, isLoading: false })
-  }
-  componentDidMount() {
-    this.getMovies()
-  }
-  render() {
-    const { isLoading, movies } = this.state
-    return (
-      <div className="container">
-        {isLoading ? (
-          <div className="loader">
-            <span className="loader__text">Loading...</span>
-          </div>
-        ) : (
-          <div className="movies">
-            {movies.map((m) => {
-              return (
-                <Movie
-                  className="movies"
-                  key={m.id}
-                  id={m.id}
-                  year={m.year}
-                  title={m.title}
-                  summary={m.summary}
-                  poster={m.medium_cover_image}
-                  genres={m.genres}
-                ></Movie>
-              )
-            })}
-          </div>
-        )}
-      </div>
-    )
-  }
+function App() {
+  return (
+    <HashRouter>
+      <Route path="/" component={Home} exact={true}></Route>
+      <Route path="/about" component={About}></Route>
+    </HashRouter>
+  )
 }
 
 export default App
---
+++ src/routes/Home.js
@@ -0,0 +1,55 @@
+import React from "react"
+import axios from "axios"
+import Movie from "../components/Movie"
+import "./Home.css"
+
+class Home extends React.Component {
+  state = {
+    isLoading: true,
+    movies: [],
+  }
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    )
+    this.setState({ movies, isLoading: false })
+  }
+  componentDidMount() {
+    this.getMovies()
+  }
+  render() {
+    const { isLoading, movies } = this.state
+    return (
+      <div className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map((m) => {
+              return (
+                <Movie
+                  className="movies"
+                  key={m.id}
+                  id={m.id}
+                  year={m.year}
+                  title={m.title}
+                  summary={m.summary}
+                  poster={m.medium_cover_image}
+                  genres={m.genres}
+                ></Movie>
+              )
+            })}
+          </div>
+        )}
+      </div>
+    )
+  }
+}
+
+export default Home
